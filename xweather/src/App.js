88c8a94c4cc06b0d42d62efd21962432c4b25d6d
abhinav kumar <abhinav2605@yahoo.com>
2024-05-06 00:10:25 +0530

    test3

--- xweather/src/App.js
+++ xweather/src/App.js
@@ -6,18 +6,19 @@ import Card from "./Card"
 
 function App() {
   const [city, setCity] = useState("");
-  const [data, setData] = useState({})
+  const [data, setData] = useState({});
+  // console.log(data)
   const getWeather=() => {
     (async () => {
       try {
         var response = await axios.get("https://api.weatherapi.com/v1/current.json?key=79bfb5c1f25b4ac686a24934230112&q="+city)
         setData(response.data);
-        console.log(response.data)
+        console.log(data)
+        
       } catch (err) {
         //return new Error("Failed to Fetch !", err);
         console.error("Failed to fetch")
     }
-      
     })();
     
   }
@@ -26,7 +27,7 @@ function App() {
       <input type="text" placeholder='Enter city name' onChange={(e)=>{setCity(e.target.value)}}/>
       <button onClick={getWeather}>Search</button>
       <div className='WeatherDetails'>
-        {data!=null?(<><Card title="Temperature" value={data.current.temp_c}/>
+        {Object.keys(data).length != 0?(<><Card title="Temperature" value={data.current.temp_c}/>
       <Card title="Humidity" value={data.current.humidity}/>
       <Card title="Condition" value={data.current.condition.text}/>
       <Card title="Wind Speed" value={data.current.wind_kph}/></>):<></>}
